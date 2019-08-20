22ae625ccd2ae1cfe14d6b4e855d2f9b8e72e872
Jakub Bryl <jakub.bryl7@gmail.com>
2019-08-20 14:25:53 +0200

    side effects tests

--- src/test/scala/pl/iterators/sealedmonad/SealedSpecs.scala
+++ src/test/scala/pl/iterators/sealedmonad/SealedSpecs.scala
@@ -1,7 +1,91 @@
 package pl.iterators.sealedmonad
 
+import pl.iterators.sealedmonad.side_effects.SealedSideEffectsInspect
+import pl.iterators.sealedmonad.side_effects.SealedSideEffectsTap
+
 class SealedSpecs extends SealedSuite {
 
   checkAll("Sealed", SealedTests[Option].tests[String, Int])
 
+  test("inspect side effect not invoked") {
+    assert(SealedSideEffectsInspect.simplyInvoke)
+  }
+  test("inspect side effect invoked") {
+    assert(SealedSideEffectsInspect.simplyNotInvoke)
+  }
+  test("inspect with valueOr with None side effect invoked") {
+    assert(SealedSideEffectsInspect.invokeOnValueOrOnNone)
+  }
+  test("inspect with valueOr with None side effect not invoked") {
+    assert(SealedSideEffectsInspect.notInvokeOnValueOrOnNone)
+  }
+  test("inspect with valueOr with Some side effect invoked") {
+    assert(SealedSideEffectsInspect.invokeOnValueOrOnSome)
+  }
+  test("inspect with valueOr with Some side effect not invoked") {
+    assert(SealedSideEffectsInspect.notInvokeOnValueOrOnSome)
+  }
+  test("inspect with positive ensure side effect invoked") {
+    assert(SealedSideEffectsInspect.invokeOnPositiveEnsure)
+  }
+  test("inspect with positive ensure side effect not invoked") {
+    assert(SealedSideEffectsInspect.notInvokeOnPositiveEnsure)
+  }
+  test("inspect with negative ensure negative side effect invoked") {
+    assert(SealedSideEffectsInspect.invokeOnNegativeEnsure)
+  }
+  test("inspect with negative ensure negative side effect not invoked") {
+    assert(SealedSideEffectsInspect.notInvokeOnNegativeEnsure)
+  }
+  test("inspect with Some valueOr with negative ensure negative side effect invoked") {
+    assert(SealedSideEffectsInspect.invokeOnSomeValueOrWithNegativeEnsure)
+  }
+  test("inspect with Some valueOr with negative ensure negative side effect not invoked") {
+    assert(SealedSideEffectsInspect.notInvokeOnSomeValueOrWithNegativeEnsure)
+  }
+  test("tap side effect invoked") {
+    assert(SealedSideEffectsTap.invokeTap)
+  }
+  test("tapWhenPositive side effect invoked") {
+    assert(SealedSideEffectsTap.invokeWithPositiveTapWhen)
+  }
+  test("tapWhenNegative side effect not invoked") {
+    assert(SealedSideEffectsTap.notInvokeWithNegativeTapWhen)
+  }
+  test("tap with valueOr with Some side effect invoked") {
+    assert(SealedSideEffectsTap.invokeOnValueOrOnSome)
+  }
+  test("tapWhenPositive with valueOr with Some side effect invoked") {
+    assert(SealedSideEffectsTap.invokeOnValueOrOnSomeWithPositiveTapWhen)
+  }
+  test("tapWhenNegative with valueOr with Some side effect not invoked") {
+    assert(SealedSideEffectsTap.notInvokeOnValueOrOnSomeWithNegativeTapWhen)
+  }
+  test("tap with valueOr with None side effect not invoked") {
+    assert(SealedSideEffectsTap.notInvokeOnValueOrOnNone)
+  }
+  test("tapWhenNPositive with valueOr with None side effect not invoked") {
+    assert(SealedSideEffectsTap.notInvokeOnValueOrOnNoneWithPositiveTapWhen)
+  }
+  test("tapWhenNegative with valueOr with None side effect not invoked") {
+    assert(SealedSideEffectsTap.notInvokeOnValueOrOnNoneWithNegativeTapWhen)
+  }
+  test("tap with positive ensure side effect invoked") {
+    assert(SealedSideEffectsTap.invokeOnPositiveEnsure)
+  }
+  test("tapWhenPositive with positive ensure side effect invoked") {
+    assert(SealedSideEffectsTap.invokeOnPositiveEnsureWithPositiveTapWhen)
+  }
+  test("tapWhenNegative with positive ensure side effect not invoked") {
+    assert(SealedSideEffectsTap.notInvokeOnPositiveEnsureWithNegativeTapWhen)
+  }
+  test("tap with negative ensure side effect invoked") {
+    assert(SealedSideEffectsTap.notInvokeOnNegativeEnsure)
+  }
+  test("tapWhenPositive with negative ensure side effect invoked") {
+    assert(SealedSideEffectsTap.notInvokeOnNegativeEnsureWithPositiveTapWhen)
+  }
+  test("tapWhenNegative with negative ensure side effect not invoked") {
+    assert(SealedSideEffectsTap.notInvokeOnNegativeEnsureWithNegativeTapWhen)
+  }
 }
---
+++ src/test/scala/pl/iterators/sealedmonad/side_effects/SealedSideEffectsInspect.scala
@@ -0,0 +1,277 @@
+package pl.iterators.sealedmonad.side_effects
+
+import cats.Id
+import pl.iterators.sealedmonad.syntax._
+
+trait SealedSideEffectsInspect {
+
+  def simplyInvoke: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    0.seal.inspect {
+      case Left(10) => invokeOther()
+      case Left(20) => invokeOther()
+      case Left(0)  => invoke()
+    }.run
+
+    invoked && !invokedOther
+  }
+
+  def simplyNotInvoke: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+    30.seal.inspect {
+      case Left(10) => invokeOther()
+      case Left(20) => invokeOther()
+      case Left(0)  => invoke()
+    }.run
+
+    !invoked && !invokedOther
+  }
+
+  def invokeOnValueOrOnNone: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    val m: Id[Option[Int]] = None
+
+    m.valueOr(0)
+      .inspect {
+        case Left(10) => invokeOther()
+        case Left(20) => invokeOther()
+        case Left(0)  => invoke()
+      }
+      .run
+
+    invoked && !invokedOther
+  }
+
+  def notInvokeOnValueOrOnNone: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    val m: Id[Option[Int]] = None
+
+    m.valueOr(30)
+      .inspect {
+        case Left(10) => invokeOther()
+        case Left(20) => invokeOther()
+        case Left(0)  => invoke()
+      }
+      .run
+
+    !invoked && !invokedOther
+  }
+
+  def invokeOnValueOrOnSome: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    val m: Id[Option[Int]] = Some(0)
+
+    m.valueOr(30)
+      .inspect {
+        case Left(10) => invokeOther()
+        case Left(20) => invokeOther()
+        case Left(30) => invokeOther()
+        case Right(0) => invoke()
+      }
+      .run
+
+    invoked && !invokedOther
+  }
+
+  def notInvokeOnValueOrOnSome: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    val m: Id[Option[Int]] = Some(0)
+
+    m.valueOr(30)
+      .inspect {
+        case Left(10) => invokeOther()
+        case Left(20) => invokeOther()
+        case Left(30) => invoke()
+      }
+      .run
+
+    !invoked && !invokedOther
+  }
+
+  def invokeOnPositiveEnsure: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    0.liftSealed
+      .ensure(_ => true, 30)
+      .inspect {
+        case Left(10) => invokeOther()
+        case Left(20) => invokeOther()
+        case Left(30) => invokeOther()
+        case Right(0) => invoke()
+      }
+      .run
+    invoked && !invokedOther
+  }
+
+  def notInvokeOnPositiveEnsure: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    0.liftSealed
+      .ensure(_ => true, 30)
+      .inspect {
+        case Left(10) => invokeOther()
+        case Left(20) => invokeOther()
+        case Left(30) => invoke()
+      }
+      .run
+
+    !invoked && !invokedOther
+  }
+
+  def invokeOnNegativeEnsure: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    10.liftSealed
+      .ensure(_ => false, 40)
+      .inspect {
+        case Left(10) => invokeOther()
+        case Left(20) => invokeOther()
+        case Left(30) => invokeOther()
+        case Left(40) => invoke()
+      }
+      .run
+
+    invoked && !invokedOther
+  }
+
+  def notInvokeOnNegativeEnsure: Boolean = {
+    var invoked: Boolean      = false
+    var invokedOther: Boolean = false
+
+    def invoke() =
+      invoked = true
+
+    def invokeOther() =
+      invokedOther = true
+
+    0.liftSealed
+      .ensure(_ => false, 30)
+      .inspect {
+        case Left(10) => invokeOther()
+        case Left(20) => invokeOther()
+        case Left(30) => invokeOther()
+        case Left(0)  => invoke()
+      }
+      .run
+
+    !invoked && invokedOther
+  }
+
+  def invokeOnSomeValueOrWithNegativeEnsure: Boolean = {
+    var shouldBeInvoked: Boolean    = false
+    var shouldNotBeInvoked: Boolean = false
+
+    def invoke() =
+      shouldBeInvoked = true
+
+    def invokeOther() =
+      shouldNotBeInvoked = true
+
+    val m: Id[Option[Int]] = Some(0)
+
+    m.valueOr(10)
+      .ensure(_ => false, 20)
+      .inspect {
+        case Left(0)  => invokeOther()
+        case Left(10) => invokeOther()
+        case Left(20) => invoke()
+      }
+      .run
+
+    shouldBeInvoked && !shouldNotBeInvoked
+  }
+
+  def notInvokeOnSomeValueOrWithNegativeEnsure: Boolean = {
+    var shouldBeInvoked: Boolean    = false
+    var shouldNotBeInvoked: Boolean = false
+
+    def invoke() =
+      shouldBeInvoked = true
+
+    def invokeOther() =
+      shouldNotBeInvoked = true
+
+    val m: Id[Option[Int]] = Some(0)
+
+    m.valueOr(10)
+      .ensure(_ => false, 80)
+      .inspect {
+        case Left(0)  => invokeOther()
+        case Left(10) => invokeOther()
+        case Left(20) => invoke()
+      }
+      .run
+
+    !shouldBeInvoked && !shouldNotBeInvoked
+  }
+}
+
+object SealedSideEffectsInspect extends SealedSideEffectsInspect
