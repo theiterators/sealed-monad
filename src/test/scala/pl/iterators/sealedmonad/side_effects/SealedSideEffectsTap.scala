32296f3b3afd91768cc8dee176f13af4ae28e2e1
Paweł Kiersznowski <pkiersznowski@iterato.rs>
2020-09-21 11:04:24 +0200

    change import

--- src/test/scala/pl/iterators/sealedmonad/side_effects/SealedSideEffectsTap.scala
+++ src/test/scala/pl/iterators/sealedmonad/side_effects/SealedSideEffectsTap.scala
@@ -2,7 +2,7 @@ package pl.iterators.sealedmonad.side_effects
 
 import cats.Id
 import pl.iterators.sealedmonad.syntax._
-import cats.instances.arraySeq._
+import cats.instances.option._
 
 trait SealedSideEffectsTap {
 
