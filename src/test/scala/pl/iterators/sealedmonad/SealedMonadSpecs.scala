85c0ca138e55066fddbdab3ec14174a2a9d54f64
Paweł Kiersznowski <pkiersznowski@iterato.rs>
2021-02-04 18:53:22 +0100

    consistency test for biSemiflatMap == leftSemiFlatMap.semiFlatMap

--- src/test/scala/pl/iterators/sealedmonad/SealedMonadSpecs.scala
+++ src/test/scala/pl/iterators/sealedmonad/SealedMonadSpecs.scala
@@ -3,7 +3,12 @@ package pl.iterators.sealedmonad
 class SealedMonadSpecs extends SealedSuite {
   import SealedTestInstances.ADT
   import cats.laws.discipline.MonadTests
-
   checkAll("Sealed", MonadTests[Sealed[Option, ?, ADT]].monad[String, String, Int])
+
+  test("biSemiflatMap consistent with leftSemiflatMap and semiflatMap") {
+    forAll { (sealedMonad: Sealed[List, Either[ADT, String], ADT], fa: ADT => List[ADT], fb: String => List[Int]) =>
+      assert(sealedMonad.biSemiflatMap(fa, fb) === (sealedMonad.leftSemiflatMap(fa).semiflatMap(fb)))
+    }
+  }
 
 }
